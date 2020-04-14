250d89bc6a365d14b8b16fe86ddc3b45259e6088
emiyl <emily@cfw.guide>
2020-04-14 23:06:44 +0100

    specific game dumping

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <malloc.h>
 #include <stdio.h>
 #include <fat.h>
+#include <sys/dirent.h>
 #include "dynamic_libs/os_functions.h"
 #include "dynamic_libs/fs_functions.h"
 #include "dynamic_libs/gx2_functions.h"
@@ -211,6 +212,279 @@ int dumpFunc(const char *path, int item, int dump_source, int dump_target, int l
 			return 1;
 }
 
+int titles_menu(int dump_source, int dump_target, int selectedItem) {
+	//!*******************************************************************
+	//!                   Initialize function pointers                   *
+	//!*******************************************************************
+	//! do OS (for acquire) and sockets first so we got logging
+	InitOSFunctionPointers();
+	InitSocketFunctionPointers();
+
+	log_init("192.168.178.3");
+	log_print("Starting launcher\n");
+
+	InitFSFunctionPointers();
+	InitVPadFunctionPointers();
+
+	log_print("Function exports loaded\n");
+
+	//!*******************************************************************
+	//!                    Initialize heap memory                        *
+	//!*******************************************************************
+	log_print("Initialize memory management\n");
+	//! We don't need bucket and MEM1 memory so no need to initialize
+	//memoryInitialize();
+
+for(int i = 0; i < MAX_CONSOLE_LINES_TV; i++)
+			consoleArrayTv[i] = NULL;
+
+for(int i = 0; i < MAX_CONSOLE_LINES_DRC; i++)
+			consoleArrayDrc[i] = NULL;
+
+	VPADInit();
+
+	int vpadError = -1;
+	VPADData vpad;
+
+	int initScreen = 1;
+
+	int stringcount = 0;
+	char **folders = NULL;
+
+	int res = IOSUHAX_Open(NULL);
+	if(res < 0)
+	{
+			console_printf(2, "IOSUHAX_open failed");
+			console_printf(1, "Ensure you are using MochaCFW");
+			sleep(1);
+			return 0;
+	}
+
+	int fsaFd = IOSUHAX_FSA_Open();
+	if(fsaFd < 0)
+	{
+			console_printf(2, "IOSUHAX_FSA_Open failed");
+			console_printf(2, "Run MochaCFW again");
+			sleep(1);
+			return 0;
+	}
+
+	fatInitDefault();
+	const char *dev_path = NULL;
+
+	char mount_path[255];
+	if (dump_source) strcpy(mount_path, "/vol/storage_usb01/usr/title/00050000");
+	else						 strcpy(mount_path, "/vol/storage_mlc01/usr/title/00050000");
+
+	int ret = mount_fs("dev", fsaFd, dev_path, mount_path);
+	if (ret < 0) return 0;
+
+	VirtualMountDevice("dev:/");
+	VirtualMountDevice("sd:/");
+	VirtualMountDevice("usb:/");
+
+	u32 size_of_folders = 0;
+
+	DIR *dir;
+	struct dirent *ent;
+	if ((dir = opendir ("dev:/")) != NULL) {
+	/* print all the files and directories within directory */
+			while ((ent = readdir (dir)) != NULL) {
+					stringcount++;
+					if (folders == NULL) {
+							folders = malloc(sizeof(char *) * stringcount);
+					} else {
+							folders = realloc(folders, sizeof(char *) * stringcount);
+					}
+					int single_string_len = strlen(ent->d_name);
+					char *single_string = malloc(sizeof(char) * single_string_len + 1);
+					strcpy(single_string, ent->d_name);
+					folders[stringcount - 1] = single_string;
+					size_of_folders++;
+			}
+			closedir (dir);
+	} else {
+			perror ("");
+			return -1;
+	}
+
+	for (int str = 0; str < stringcount; str++) {
+			puts(folders[str]);
+	}
+
+	int checkBox[(sizeof(folders) / 4)];
+
+	u32 i;
+	for(i = 0; i < (sizeof(folders) / 4); i++) {
+		checkBox[i] = 0;
+	}
+
+	while(1)
+	{
+			//! update only at 50 Hz, thats more than enough
+			VPADRead(0, &vpad, 1, &vpadError);
+
+			if(initScreen)
+			{
+					initScreen = 0;
+
+					//! free memory
+					for(int i = 0; i < MAX_CONSOLE_LINES_TV; i++)
+					{
+							if(consoleArrayTv[i])
+									free(consoleArrayTv[i]);
+							consoleArrayTv[i] = 0;
+					}
+
+					for(int i = 0; i < MAX_CONSOLE_LINES_DRC; i++)
+					{
+							if(consoleArrayDrc[i])
+									free(consoleArrayDrc[i]);
+							consoleArrayDrc[i] = 0;
+					}
+
+					// Clear screens
+					OSScreenClearBufferEx(0, 0);
+					OSScreenClearBufferEx(1, 0);
+
+
+					console_print_pos(0, 1, "-- dumpling v0.5 pre-release by emiyl --");
+					console_print_pos(0, 2, "Source: %s // Target: %s", (dump_source) ? "USB" : "System", (dump_target) ? "USB" : "SD");
+
+					console_print_pos(0, 4, "Press A to select and START to begin dumping.");
+					console_print_pos(0, 5, "Use L/R to change source and ZL/ZR to change target.");
+
+					u32 i;
+
+					for(i = 0; i < size_of_folders; i++)
+					{
+							if (checkBox[i]) console_print_pos(0, 7 + i, "[x]");
+							else console_print_pos(0, 7 + i, "[ ]");
+
+							if (selectedItem == (int)i) console_print_pos(4, 7 + i, "> %s", folders[i]);
+							else console_print_pos(4, 7 + i, "  %s", folders[i]);
+					}
+
+					console_print_pos(0, 16, "Hold B to cancel. Press X to return to main menu.");
+
+					// Flip buffers
+					OSScreenFlipBuffersEx(0);
+					OSScreenFlipBuffersEx(1);
+			}
+
+			if(vpadError == 0 && ((vpad.btns_d | vpad.btns_h) & VPAD_BUTTON_DOWN))
+			{
+					selectedItem = (selectedItem + 1) % size_of_folders;
+					initScreen = 1;
+					usleep(150000);
+			}
+
+			if(vpadError == 0 && ((vpad.btns_d | vpad.btns_h) & VPAD_BUTTON_UP))
+			{
+					selectedItem--;
+					if(selectedItem < 0)
+							selectedItem =  size_of_folders - 1;
+					initScreen = 1;
+					usleep(150000);
+			}
+
+			if(vpadError == 0 && ((vpad.btns_d | vpad.btns_h) & (VPAD_BUTTON_R | VPAD_BUTTON_RIGHT | VPAD_BUTTON_L | VPAD_BUTTON_LEFT)))
+			{
+					u32 i;
+					for(i = 0; i < size_of_folders; i++) {
+						checkBox[i] = 0;
+					}
+					dump_source = !dump_source;
+					if (selectedItem > 4)
+						selectedItem = 3;
+					initScreen = 1;
+					usleep(150000);
+			}
+
+			if(vpadError == 0 && ((vpad.btns_d | vpad.btns_h) & (VPAD_BUTTON_ZR | VPAD_BUTTON_ZL)))
+			{
+					dump_target = !dump_target;
+					initScreen = 1;
+					usleep(150000);
+			}
+
+			if(vpadError == 0 && ((vpad.btns_d | vpad.btns_h) & VPAD_BUTTON_A))
+			{
+					checkBox[selectedItem] = !checkBox[selectedItem];
+					initScreen = 1;
+					usleep(150000);
+			}
+
+			if(vpadError == 0 && ((vpad.btns_d | vpad.btns_h) & VPAD_BUTTON_X))
+			{
+					fatUnmount("sd");
+					fatUnmount("usb");
+					IOSUHAX_sdio_disc_interface.shutdown();
+					IOSUHAX_usb_disc_interface.shutdown();
+
+					unmount_fs("dev");
+					UnmountVirtualPaths();
+
+					IOSUHAX_FSA_Close(fsaFd);
+					IOSUHAX_Close();
+					return 1;
+			}
+
+			if(vpadError == 0 && ((vpad.btns_d | vpad.btns_h) & VPAD_BUTTON_PLUS))
+			{
+					u32 i;
+					for(i = 0; i < size_of_folders; i++)
+					{
+							if (checkBox[i]) {
+									char selection_path[255];
+									snprintf(selection_path, sizeof(selection_path), "storage_%s01:/usr/title/00050000/%s", (dump_source) ? "usb" : "mlc", folders[i]);
+									dumpFunc(selection_path, i, dump_source, dump_target, 1);
+							}
+							initScreen = 1;
+					}
+			}
+
+			if(vpadError == 0 && ((vpad.btns_d | vpad.btns_h) & VPAD_BUTTON_HOME))
+			{
+					fatUnmount("sd");
+					fatUnmount("usb");
+					IOSUHAX_sdio_disc_interface.shutdown();
+					IOSUHAX_usb_disc_interface.shutdown();
+
+					unmount_fs("dev");
+					UnmountVirtualPaths();
+
+					IOSUHAX_FSA_Close(fsaFd);
+					IOSUHAX_Close();
+					return 1;
+			}
+
+	usleep(50000);
+	}
+
+	//! free memory
+	for(int i = 0; i < MAX_CONSOLE_LINES_TV; i++)
+	{
+			if(consoleArrayTv[i])
+					free(consoleArrayTv[i]);
+	}
+
+	for(int i = 0; i < MAX_CONSOLE_LINES_DRC; i++)
+	{
+			if(consoleArrayDrc[i])
+					free(consoleArrayDrc[i]);
+	}
+
+	for (int str = 0; str < stringcount; str++) {
+			free(folders[str]);
+	}
+	free(folders);
+
+	log_printf("Release memory\n");
+	//memoryRelease();
+	log_deinit();
+}
+
 /* Entry point */
 int Menu_Main(void)
 {
@@ -363,7 +637,7 @@ int Menu_Main(void)
             OSScreenClearBufferEx(1, 0);
 
 
-            console_print_pos(0, 1, "-- dumpling v0.4 by emiyl --");
+            console_print_pos(0, 1, "-- dumpling v0.5 pre-release by emiyl --");
             console_print_pos(0, 2, "Source: %s // Target: %s", (dump_source) ? "USB" : "System", (dump_target) ? "USB" : "SD");
 
             console_print_pos(0, 4, "Press A to select and START to begin dumping.");
@@ -380,7 +654,7 @@ int Menu_Main(void)
                 else console_print_pos(4, 7 + i, "  %s", (dump_source) ? usb_selection_text[i] : mlc_selection_text[i]);
             }
 
-            console_print_pos(0, 16, "Hold B to cancel dumping.");
+            console_print_pos(0, 16, "Hold B to cancel. Press X to dump invidual titles.");
 
             // Flip buffers
             OSScreenFlipBuffersEx(0);
@@ -428,6 +702,14 @@ int Menu_Main(void)
 						checkBox[selectedItem] = !checkBox[selectedItem];
 						initScreen = 1;
             usleep(150000);
+        }
+
+        if(vpadError == 0 && ((vpad.btns_d | vpad.btns_h) & VPAD_BUTTON_X))
+        {
+						usleep(150000);
+						titles_menu(dump_source, dump_target, selectedItem);
+						initScreen = 1;
+						usleep(150000);
         }
 
         if(vpadError == 0 && ((vpad.btns_d | vpad.btns_h) & VPAD_BUTTON_PLUS))
